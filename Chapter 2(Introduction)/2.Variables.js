6725f4d1429c00185ec77142081d222604240928
Shuvo Kumar Das <96796027+shuvookd@users.noreply.github.com>
2022-08-15 18:41:47 +0600

    Create 2.Variables.js

---
+++ Chapter 2(Introduction)/2.Variables.js
@@ -0,0 +1,12 @@
+//Declering variable
+
+//Variable cannot start with capital letter.
+// name is a reserved word
+var name = 'Shuvo'
+var age = 23
+
+console.log(name)
+console.log(age)
+
+console.log(name + " Kumar Das")
+console.log("He is " + age + " years old")
